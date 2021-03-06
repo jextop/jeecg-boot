b2e50771180b70592132235b916a81a95949e85c
dingxl <dingxl18@qq.com>
2021-03-06 08:42:22 +0800

    DevOps：增加脚本，将指定提交发布到master分支，推送gitee和github开源代码库

---
+++ push_master.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "commit:" $1
+
+git checkout master
+git cherry-pick $1
+git status
+
+git push origin master
+git push gitee master
+git push github master
+
+git checkout rel
+git status
