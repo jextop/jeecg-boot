74da43770992e15e786cd1e67b46f8f00f5f3b51
dingxl <dingxl18@qq.com>
2021-03-13 11:44:16 +0800

    功能开发：添加多租户初始化数据，SQL脚本

---
+++ jeecg-boot/jeecg-boot-module-system/src/main/resources/db/migration/V0.1.3__init_sys_tenant.sql
@@ -0,0 +1,5 @@
+truncate sys_tenant;
+
+INSERT INTO `sys_tenant` (`id`, `name`, `create_time`, `create_by`, `begin_date`, `end_date`, `status`) VALUES
+(1,	'平台运营',	'2020-07-10 15:43:32',	'admin',	'2021-03-13 10:47:55',	'2099-03-13 10:47:55',	1),
+(2,	'平台测试',	'2021-03-13 10:49:24',	'admin',	'2021-03-13 10:49:02',	'2099-03-13 10:49:02',	1);
